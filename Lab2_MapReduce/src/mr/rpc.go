7e02f24b6fe375b80aa5adf93af2642292778768
Arv <arvin.allahbakhsh@gmail.com>
2025-11-27 19:56:40 +0100

    testing basic part

---
+++ Lab2_MapReduce/src/mr/rpc.go
@@ -0,0 +1,47 @@
+package mr
+
+//
+// RPC definitions.
+//
+// remember to capitalize all names.
+//
+
+import (
+	"os"
+	"strconv"
+)
+
+//
+// example to show how to declare the arguments
+// and reply for an RPC.
+//
+
+type ExampleArgs struct {
+	X int
+}
+
+type ExampleReply struct {
+	Y int
+}
+
+// Add your RPC definitions here.
+
+// Task request - worker asks coordinator for a task
+type RequestTaskArgs struct {
+	WorkerID int
+}
+
+type RequestTaskReply struct {
+	TaskType string // "Map", "Reduce", or "Wait" or "Exit"
+	TaskID   int
+}
+
+// Cook up a unique-ish UNIX-domain socket name
+// in /var/tmp, for the coordinator.
+// Can't use the current directory since
+// Athena AFS doesn't support UNIX-domain sockets.
+func coordinatorSock() string {
+	s := "/var/tmp/5840-mr-"
+	s += strconv.Itoa(os.Getuid())
+	return s
+}
